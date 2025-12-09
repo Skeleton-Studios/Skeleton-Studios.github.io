9edd1176ee40b999f80f0a630b6094fe44fc52ee
Skull <33642481+Skull220@users.noreply.github.com>
2025-12-09 15:31:28 -0500

    reverted formatting change, will standardize this navbar later

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -7,16 +7,16 @@ class SpecialHeader extends HTMLElement{
                 <button class="dropbtn-active" onclick="ClickFunction()">GAMES</button>
                 <div class="dropdown-content">
                     <a href="On_Your_Hands.html#main">ON YOUR HANDS</a>
-                    <a href="Welcome_To_Ooblterra.html#main">Welcome To Ooblterra!</a>
-                    <a href="Delta_Perspective.html#main">Delta Perspective</a>
-                    <a href="Project_Freezer.html">Upcoming Projects...</a>
+                    <a href="Welcome_To_Ooblterra.html#main">WELCOME TO OOBLTERRA</a>
+                    <a href="Delta_Perspective.html#main">DELTA PERSPECTIVE</a>
+                    <a href="Project_Freezer.html">UPCOMING PROJECTS</a>
                 </div>
             </div> 
             <div id= "hiddenDropDown1" class="collapsed">
                 <a href="On_Your_Hands.html#main" class="uncollapsed">ON YOUR HANDS</a>
-                <a href="Welcome_To_Ooblterra.html" class="uncollapsed">Welcome To Ooblterra!</a>
-                <a href="Delta_Perspective.html" class="uncollapsed">Delta Perspective</a>
-                <a href="Project_Freezer.html" class="uncollapsed">Upcoming Projects...</a>
+                <a href="Welcome_To_Ooblterra.html" class="uncollapsed">WELCOME TO OOBLTERRA</a>
+                <a href="Delta_Perspective.html#main">DELTA PERSPECTIVE</a>
+                <a href="Project_Freezer.html">UPCOMING PROJECTS</a>
             </div>
             <a href="index.html">ABOUT</a>
             <a href="devlog.html">NEWSLETTER</a>
