650991c4654a9c95f5c10f25f1f000c51a6e5b68
Skull <33642481+Skull220@users.noreply.github.com>
2025-12-09 17:35:55 -0500

    added uncollapsed tag to these two

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -15,8 +15,8 @@ class SpecialHeader extends HTMLElement{
             <div id= "hiddenDropDown1" class="collapsed">
                 <a href="On_Your_Hands.html#main" class="uncollapsed">ON YOUR HANDS</a>
                 <a href="Welcome_To_Ooblterra.html" class="uncollapsed">WELCOME TO OOBLTERRA</a>
-                <a href="Delta_Perspective.html#main">DELTA PERSPECTIVE</a>
-                <a href="Project_Freezer.html">UPCOMING PROJECTS</a>
+                <a href="Delta_Perspective.html#main" class="uncollapsed">DELTA PERSPECTIVE</a>
+                <a href="Project_Freezer.html" class="uncollapsed">UPCOMING PROJECTS</a>
             </div>
             <a href="index.html">ABOUT</a>
             <a href="devlog.html">NEWSLETTER</a>
